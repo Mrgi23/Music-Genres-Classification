6f5b8aba1365fd681956bb284e9d93373ef2f276
Aleksa Markovic <aleksa.markovic96@outlook.com>
2025-03-24 01:03:40 +0100

    feat: Added dataset downloader

---
+++ inc/downloader.h
@@ -0,0 +1,44 @@
+#ifndef DOWNLOADER_H
+#define DOWNLOADER_H
+
+#ifdef __cplusplus
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+namespace fs = std::filesystem;
+
+class Downloader {
+    private:
+        fs::path root;
+        std::string url;
+        fs::path zipFile;
+
+        static size_t writeToFile(void * ptr, size_t size, size_t nmemb, void * userdata) {
+            // Cast user data.
+            std::ofstream * out = static_cast<std::ofstream*>(userdata);
+
+            // Write incoming chunk.
+            out->write(static_cast<char*>(ptr), size * nmemb);
+            return size * nmemb;
+        }
+
+        bool datasetExists();
+        void datasetDownload();
+        void datasetExtract();
+    public:
+        Downloader(
+            fs::path root = "../resources",
+            std::string url = "https://www.kaggle.com/api/v1/datasets/download/andradaolteanu/gtzan-dataset-music-genre-classification"
+        ) : root(root), url(url), zipFile(root / "dataset.zip") { fs::create_directories(root); }
+        ~Downloader() {}
+
+        inline fs::path const getRoot() { return root; }
+
+        void run();
+};
+
+#endif
+
+#endif
