af36761a15bae72add8837c45042c87a5ba9246c
Aleksa Markovic <aleksa.markovic96@outlook.com>
2025-03-24 01:04:34 +0100

    test: Added tests for dataset downloader

---
+++ tests/cpp/unit/testUnitDownloader.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "downloader.h"
+
+using namespace std;
+
+TEST(TestDownloader, testDownloader) {
+    // Define the test object.
+    Downloader downloader = Downloader("../../resources");
+
+    // Define the expected result.
+    uint nFoldersExpected = 10U;
+    uint nFilesExpected = 999U;
+
+    // Compute the result.
+    fs::path root = downloader.getRoot();
+    downloader.run();
+    downloader.run();
+
+    // Test the result.
+    uint nFolders = 0U;
+    uint nFiles = 0U;
+    for (auto& entry : fs::recursive_directory_iterator(root)) {
+        if (fs::is_directory(entry)) { nFolders++; }
+        else if (entry.path().extension() == ".wav") { nFiles++; }
+    }
+    ASSERT_EQ(nFolders, nFoldersExpected) << "Invalid number of the dataset classes.";
+    ASSERT_EQ(nFiles, nFilesExpected) << "Invalid number of the dataset samples.";
+}
---
+++ tests/python/unit/test_unit_downloader.py
@@ -0,0 +1,28 @@
+import shutil
+from downloader import Downloader
+
+def test_downloader():
+    # Define the test object.
+    downloader = Downloader("./resources")
+
+    # Define the expected result.
+    n_folders_expected = 10
+    n_files_expected = 999
+
+    # Compute the result.
+    root = downloader.root
+    for item in root.iterdir():
+        if item.is_dir():
+            shutil.rmtree(item)
+    downloader.run()
+
+    # Test the result.
+    n_folders = 0
+    n_files = 0
+    for entry in root.rglob("*"):
+        if entry.is_dir():
+            n_folders += 1
+        if entry.is_file() and entry.suffix == ".wav":
+            n_files += 1
+    assert(n_folders == n_folders_expected), "Invalid number of the dataset classes."
+    assert(n_files == n_files_expected), "Invalid number of the dataset samples."
